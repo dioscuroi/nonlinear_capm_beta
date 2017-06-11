601f9218dda17a1205a0d777e7a8f0fc614fd771
Jaehoon Lee <dioscuroi@gmail.com>
2017-06-11 14:16:13 +1000

    load sql tables and write to STATA files
    load sql tables and write to STATA files

--- stata/write_beta_stats_to_csv.py
+++ stata/write_beta_stats_to_csv.py
@@ -4,15 +4,29 @@ from nonlinear_capm_beta.estimation.DataLoader import DataLoader
 
 loader = DataLoader(connect=True)
 
-# query = "select year, rank, permno, beta_average, beta_delay, beta_convexity \
-#   from beta_parameters_stocks where rank <= 500 and beta_average is not null"
+######################################################
+# Load and write beta_parameters_stocks table
+######################################################
 
 query = "select year, rank, permno, beta_average, beta_delay, beta_convexity \
   from beta_parameters_stocks where beta_average is not null"
 
 beta_stats = loader.sql_query_select(query)
 
-loader.close()
+beta_stats.to_stata('beta_stats_roll.dta', write_index=False)
 
-beta_stats.to_csv('beta_stats.csv', index=False)
+
+######################################################
+# Load and write beta_parameters_stocks_full_periods table
+######################################################
+
+query = "select permno, obs, beta_average, beta_delay, beta_convexity \
+  from beta_parameters_stocks_full_periods where beta_average is not null"
+
+beta_stats = loader.sql_query_select(query)
+
+beta_stats.to_stata('beta_stats_full.dta', write_index=False)
+
+
+loader.close()
 
