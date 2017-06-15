0faf7b1dc9f24cf606cc251f1c71c348a0ba2a18
Jaehoon Lee <dioscuroi@gmail.com>
2017-06-15 11:18:12 +1000

    Push Docker container to N.Virginia
    Push Docker container to N.Virginia

--- build_containers.sh
+++ build_containers.sh
@@ -14,12 +14,12 @@ docker build -t awsbatch/beta_estimation .
 #aws ecr get-login --region us-east-1
 #aws ecr get-login --region us-west-2
 
-docker tag awsbatch/beta_estimation:latest 026533969070.dkr.ecr.us-west-2.amazonaws.com/awsbatch/beta_estimation:latest
+docker tag awsbatch/beta_estimation:latest 026533969070.dkr.ecr.us-east-1.amazonaws.com/awsbatch/beta_estimation:latest
 
 echo ""
 echo "* Pushing the container to AWS"
 
-docker push 026533969070.dkr.ecr.us-west-2.amazonaws.com/awsbatch/beta_estimation:latest
+docker push 026533969070.dkr.ecr.us-east-1.amazonaws.com/awsbatch/beta_estimation:latest
 
 echo ""
 echo "* Deleting dangling Docker images"
