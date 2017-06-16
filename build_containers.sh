3df29126216027a283467a71b3b3939068a3f107
Jaehoon Lee <dioscuroi@gmail.com>
2017-06-16 19:27:53 +1000

    Get credential from AWS ECR automatically
    Get credential from AWS ECR automatically

--- build_containers.sh
+++ build_containers.sh
@@ -11,8 +11,8 @@ docker build -t awsbatch/beta_estimation .
 # Register the container to AWS ECR repository from the AWS Management Console
 
 # Get authorized using the AWS command line tool
-#aws ecr get-login --region us-east-1
-#aws ecr get-login --region us-west-2
+#eval $(aws ecr get-login --region us-west-2)
+eval $(aws ecr get-login --region us-east-1)
 
 docker tag awsbatch/beta_estimation:latest 026533969070.dkr.ecr.us-east-1.amazonaws.com/awsbatch/beta_estimation:latest
 
